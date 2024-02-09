64513fd877563247c255d734ffb143e1b52a8e93
momori-nakano <momori.nakano@gmail.com>
2024-02-08 18:21:53 -0800

    Add pdf-extract

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    let path = std::env::args().nth(1).expect("'path' must be provided.");
+    let out = pdf_extract::extract_text(path).unwrap();
+    println!("{out}");
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let out = pdf_extract::extract_text("dummy.pdf").unwrap();
+        assert_eq!("\n\nDummy PDF file", out);
+    }
 }
