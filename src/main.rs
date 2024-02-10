7b572936947f5c42c33696096f1bddf50bc028a5
Momori Nakano <90558309+momori256@users.noreply.github.com>
2024-02-09 19:57:20 -0800

    Add ollama (#4)
    * Add ollama
    
    * Update GitHub workflow for ollama
    
    * Remove ollama when testing

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,8 @@
-fn main() {
-    let path = std::env::args().nth(1).expect("'path' must be provided.");
-    let out = pdf_extract::extract_text(path).unwrap();
-    println!("{out}");
-}
+use pdf_summarizer::*;
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let out = pdf_extract::extract_text("dummy.pdf").unwrap();
-        assert_eq!("\n\nDummy PDF file", out);
-    }
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let path = std::env::args().nth(1).expect("'path' must be provided.");
+    summarize(std::path::Path::new(&path)).await?;
+    Ok(())
 }
