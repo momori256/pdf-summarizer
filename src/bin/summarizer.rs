c4301790f862b2d8429a454e589aa08073042c1d
momori-nakano <momori.nakano@gmail.com>
2024-02-10 19:27:35 -0800

    Add summarizer

---
+++ src/bin/summarizer.rs
@@ -0,0 +1,20 @@
+use ollama_rs::{generation::completion::request::GenerationRequest, Ollama};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    const USAGE: &str = "Usage: ./summarizer <pdf_path> <model>";
+
+    let mut args = std::env::args().skip(1);
+    let pdf_path = args.next().expect(USAGE);
+    let model = args.next().expect(USAGE);
+
+    let ollama = Ollama::default();
+    let pdf = pdf_extract::extract_text(pdf_path)?;
+    let prompt = format!("Summarize the following text from a PDF file:\n{pdf}");
+
+    let request = GenerationRequest::new(model, prompt);
+    let response = ollama.generate(request).await?;
+    println!("{}", response.response);
+
+    Ok(())
+}
