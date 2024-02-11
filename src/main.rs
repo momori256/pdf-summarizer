5cf09aa22057b23dc82d9c198e1925d0677fead4
Momori Nakano <90558309+momori256@users.noreply.github.com>
2024-02-10 18:55:12 -0800

    Add anyhow for better error message (#10)
    * Add anyhow
    
    * Remove chat test

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
+use anyhow::Context;
 use clap::Parser;
 use pdf_summarizer::args::{Args, Commands};
 use pdf_summarizer::ollama_api;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> anyhow::Result<()> {
     let Args { model, command } = Args::parse();
     match command {
-        Commands::Summarize { path } => ollama_api::summarize(&path, &model).await?,
-        Commands::Name { path } => ollama_api::name(&path, &model).await?,
-        Commands::Chat => ollama_api::chat().await?,
-    }
+        Commands::Summarize { path } => ollama_api::summarize(&path, &model)
+            .await
+            .context("Summarize failed.")?,
+        Commands::Name { path } => ollama_api::name(&path, &model)
+            .await
+            .context("Name failed.")?,
+        Commands::Chat => ollama_api::chat().await.context("Chat failed.")?,
+    };
     Ok(())
 }
