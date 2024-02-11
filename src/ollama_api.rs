e54d5cb4dfa83f9494d8211faf2258bf6313ace1
Momori Nakano <90558309+momori256@users.noreply.github.com>
2024-02-10 16:02:17 -0800

    Add use command (#8)
    * Divide implementation of use command
    
    * Implement tests for chat

--- src/ollama_api.rs
+++ src/ollama_api.rs
@@ -3,8 +3,8 @@ use ollama_rs::{
     generation::completion::{request::GenerationRequest, GenerationContext, GenerationResponse},
     Ollama,
 };
-use std::io::Write;
-use std::path::Path;
+use std::path::PathBuf;
+use std::{path::Path, pin::Pin};
 use tokio_stream::StreamExt;
 
 type AppResult = Result<(), Box<dyn std::error::Error>>;
@@ -53,7 +53,30 @@ async fn ask_stream<T: std::io::Write>(
     if let Some(context) = context {
         req = req.context(context.clone());
     }
-    let mut stream = ollama.generate_stream(req).await?;
+    let mut stream = if cfg!(test) {
+        let stream = tokio_stream::iter(vec![Ok::<
+            Vec<GenerationResponse>,
+            ollama_rs::error::OllamaError,
+        >(vec![
+            GenerationResponse {
+                model: req.model_name.clone(),
+                created_at: Utc::now().to_string(),
+                response: "Dummy ".to_string(),
+                done: false,
+                final_data: None,
+            },
+            GenerationResponse {
+                model: req.model_name.clone(),
+                created_at: Utc::now().to_string(),
+                response: "Response".to_string(),
+                done: true,
+                final_data: None,
+            },
+        ])]);
+        Pin::new(Box::new(stream))
+    } else {
+        ollama.generate_stream(req).await?
+    };
     while let Some(Ok(responses)) = stream.next().await {
         for res in responses {
             out.write_all(res.response.as_bytes())?;
@@ -88,29 +111,84 @@ pub async fn name(pdf_path: &Path, model: &str) -> AppResult {
 }
 
 pub async fn chat() -> AppResult {
-    chat_internal(std::io::stdin()).await
+    chat_internal(std::io::stdin().lock(), &mut std::io::stdout()).await
 }
 
-async fn chat_internal<T: std::io::Read>(mut input: T) -> AppResult {
+async fn chat_internal<R: std::io::BufRead, W: std::io::Write>(
+    mut input: R,
+    out: &mut W,
+) -> AppResult {
     let ollama = Ollama::default();
     let model = "orca-mini:latest".to_string();
-    let mut out = std::io::stdout();
-    let mut context = None;
+
+    let mut context: Option<GenerationContext> = None;
+    let mut pdf: Option<String> = None;
 
     loop {
         out.write_all("> ".as_bytes())?;
         out.flush()?;
 
         let mut prompt = String::new();
-        input.read_to_string(&mut prompt)?;
+        input.read_line(&mut prompt)?;
         let prompt = prompt.trim();
+
         if prompt == ":exit" {
             break;
         }
 
-        ask_stream(&ollama, &model, prompt, &mut context, &mut out).await?;
+        if prompt.starts_with(":use") {
+            match parse_use_command(prompt) {
+                Ok(content) => {
+                    pdf = Some(content);
+                    out.write_all("PDF was Successfully loaded. Now {pdf} will be replaced by the content of the PDF.\n".as_bytes())?;
+                    out.flush()?;
+                }
+                Err(e) => {
+                    out.write_all(format!("Failed to load PDF: {e:?}\n").as_bytes())?;
+                    out.flush()?;
+                }
+            };
+            continue;
+        }
+
+        let prompt = match substitue_pdf(prompt, &pdf) {
+            Ok(prompt) => prompt,
+            Err(e) => {
+                out.write_all(e.as_bytes())?;
+                out.flush()?;
+                continue;
+            }
+        };
+        ask_stream(&ollama, &model, &prompt, &mut context, out).await?;
     }
     Ok(())
+}
+
+fn parse_use_command(prompt: &str) -> Result<String, Box<dyn std::error::Error>> {
+    let (_, path) = prompt.split_once(' ').ok_or(":use <path>.")?;
+    let path = normalize_path(Path::new(path))?;
+    let content = pdf_extract::extract_text(path)?;
+    Ok(content)
+}
+
+fn substitue_pdf(prompt: &str, pdf: &Option<String>) -> Result<String, String> {
+    const PDF: &str = "{pdf}";
+    if !prompt.contains(PDF) {
+        return Ok(prompt.to_string());
+    }
+    match pdf {
+        Some(pdf) => Ok(prompt.replace(PDF, pdf)),
+        None => Err("Specify a PDF with ':use <path>'\n".to_string()),
+    }
+}
+
+fn normalize_path(path: &Path) -> Result<PathBuf, Box<dyn std::error::Error>> {
+    if let Ok(path) = path.strip_prefix("~") {
+        let home = std::env::var("HOME")?;
+        let home = Path::new(&home);
+        return Ok(home.join(path));
+    }
+    Ok(path.to_path_buf())
 }
 
 #[cfg(test)]
@@ -147,7 +225,89 @@ mod tests {
     #[tokio::test]
     async fn exit_from_chat() -> AppResult {
         let input = Cursor::new(":exit");
-        chat_internal(input).await?;
+        let mut output = Cursor::new(vec![0; 4]);
+        chat_internal(input, &mut output).await?;
+        assert_eq!(vec![62, 32, 0, 0], output.into_inner());
         Ok(())
+    }
+
+    #[tokio::test]
+    async fn chat_respond_with_something() -> AppResult {
+        let input = Cursor::new("Why is the sky blue?\n:exit");
+        let mut output = Cursor::new(vec![0; 20]);
+        chat_internal(input, &mut output).await?;
+        assert_eq!(
+            "> Dummy Response\n> ",
+            String::from_utf8(output.into_inner())
+                .unwrap()
+                .trim_end_matches(|c| c == '\0')
+        );
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn chat_use_pdf() -> AppResult {
+        let input = Cursor::new(":use dummy.pdf\n{pdf}\n:exit");
+        let mut output = Cursor::new(vec![0; 1024]);
+        chat_internal(input, &mut output).await?;
+        assert_eq!(
+            "> PDF was Successfully loaded. Now {pdf} will be replaced by the content of the PDF.\n> Dummy Response\n> ",
+            String::from_utf8(output.into_inner())
+                .unwrap()
+                .trim_end_matches(|c| c == '\0')
+        );
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn chat_cannot_use_pdf_before_use() -> AppResult {
+        let input = Cursor::new("{pdf}\n:exit");
+        let mut output = Cursor::new(vec![0; 1024]);
+        chat_internal(input, &mut output).await?;
+        assert_eq!(
+            "> Specify a PDF with ':use <path>'\n> ",
+            String::from_utf8(output.into_inner())
+                .unwrap()
+                .trim_end_matches(|c| c == '\0')
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn paser_use_command_can_parse_dummy_pdf() {
+        let prompt = ":use dummy.pdf";
+        let content = parse_use_command(prompt).unwrap();
+        assert_eq!("\n\nDummy PDF file", content);
+    }
+
+    #[test]
+    fn paser_use_command_fail_without_path() {
+        let prompt = ":use";
+        let result = parse_use_command(prompt);
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn substitute_pdf_succeeds_with_pdf() {
+        let prompt = "Summarize the following text:\n{pdf}";
+        let pdf = Some("Dummy PDF file".to_string());
+        let result = substitue_pdf(prompt, &pdf).unwrap();
+        assert_eq!("Summarize the following text:\nDummy PDF file", result);
+    }
+
+    #[test]
+    fn substitute_pdf_fails_with_pdf() {
+        let prompt = "Summarize the following text:\n{pdf}";
+        let pdf = None;
+        let result = substitue_pdf(prompt, &pdf);
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn normalize_path_expands_tilde() {
+        let path = Path::new("~/a/dummy.pdf");
+        let path = normalize_path(path);
+        let home = std::env::var("HOME").unwrap();
+        assert_eq!(Path::new(&format!("{home}/a/dummy.pdf")), path.unwrap());
     }
 }
